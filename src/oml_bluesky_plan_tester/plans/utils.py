5d339757e3bebbea7ba9cc8a6acc43a163c423ee
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-02-05 11:56:29 +0000

    Add plans.utils.py with get_plan skeleton

---
+++ src/oml_bluesky_plan_tester/plans/utils.py
@@ -0,0 +1,10 @@
+def get_plan(plan_name: str):
+    """Util function to return Bluesky plan with given name.
+
+    Args:
+        plan_name: str name of plan to return
+
+    Returns:
+        A Bluesky plan
+    """
+    raise NotImplementedError
